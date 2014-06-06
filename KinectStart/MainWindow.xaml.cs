e73c43d189ece08344ee1c4ba529e620b5b83e8f
jaHoltz <jaholtz@vassar.edu>
2014-06-06 12:51:50 -0400

    Initial
    Ready

---
+++ KinectStart/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Kinect;
+
+namespace KinectStart
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// Main handling for entire project
+  /// Displays data from the kinect
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    //Global variables
+    KinectSensor sensor;
+    WriteableBitmap depthBitmap;
+    WriteableBitmap sliceBitmap;
+
+
+    public MainWindow()
+    {
+      InitializeComponent();
+      //Start the Event Handlers on initialize
+      this.Loaded += MainWindow_Loaded;
+      this.Closing += MainWindow_Closing;
+      this.MouseDown += MainWindow_MouseDown;
+    }
+
+    //Event handler for main window loading
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+      //Find a kinect and just pick one to be the sensor for our program
+      //assumes we aren't using multiple kinects
+      foreach (var curSensor in KinectSensor.KinectSensors)
+      {
+        if (curSensor.Status == KinectStatus.Connected)
+        {
+          sensor = curSensor;
+          break;
+        }
+      }
+
+      //Setup our sensor and get info if not null
+      if (sensor != null)
+      {
+        //Enables the depth and color streams (we want these)
+        sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+        sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+        //Enables the skeleton tracker (I think we want to play with this)
+        //Also for testing player identification this is necessary
+        sensor.SkeletonStream.Enable();
+        sensor.AllFramesReady += this.sensor_AllFramesReady;
+
+        //Start it if we still have a sensor
+        try
+        {
+          this.sensor.Start();
+
+          //Initialize a pair of writeable bitmaps after starting the sensor
+          depthBitmap = new WriteableBitmap
+                (this.sensor.DepthStream.FrameWidth,
+                this.sensor.DepthStream.FrameHeight,
+                96.0, 96.0, PixelFormats.Bgr32, null);
+
+          sliceBitmap = new WriteableBitmap
+              (this.sensor.DepthStream.FrameWidth,
+              this.sensor.DepthStream.FrameHeight,
+              96.0, 96.0, PixelFormats.Bgr32, null);
+
+          //depthDisplay.Source = depthBitmap;
+          depthSliceDisplay.Source = sliceBitmap;
+
+        }
+        catch (IOException)
+        {
+          this.sensor = null;
+        }
+      }
+
+    }
+
+    /*
+     * Event which triggers when all frames are ready from the kinect
+     * (will operate at the speed of the slowest camera)
+     **/
+    private void sensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+    {
+      using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+      {
+        using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+        {
+
+          if (depthFrame != null)
+          {
+            // Display an actual slice 
+            // (smaller range should only display things within that range)
+            //Range can be tweaked via sliders on application
+            depthFrame.depthSlice(sliceBitmap, (int)minSlider.Value, (int)maxSlider.Value);
+          }
+
+          if (colorFrame != null)
+          {
+            //Initializes a byte array to the size 
+            //of colorFrame's pixelDataLength
+            byte[] colorPixels = new byte[colorFrame.PixelDataLength];
+            colorFrame.CopyPixelDataTo(colorPixels);
+            //Number of pixels in a row (based on bgr32 format)
+            int stride = colorFrame.Width * 4;
+            /*
+            * Creates a bitmapsource based on the colorFrame coming 
+            * from the kinect (in bgr32 using our calculated stride)
+            * and displays it to the image frame named colorDisplay
+            **/
+            colorDisplay.Source =
+                BitmapSource.Create
+                (colorFrame.Width, colorFrame.Height,
+                96, 96, PixelFormats.Bgr32, null, colorPixels, stride);
+          }
+        }
+      }
+    }
+
+    void MainWindow_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+      this.DragMove();
+    }
+
+    private void CloseBtnClick(object sender, RoutedEventArgs e)
+    {
+      this.Close();
+    }
+
+    // Event handler for the closing of the main window (stops the kinect)
+    private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+      if (sensor != null)
+      {
+        sensor.Stop();
+      }
+    }
+
+
+  }
+}
---
+++ KinectStart/depthHelper.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Kinect;
+
+namespace KinectStart
+{
+  /// <summary>
+  /// Contains helper functions used to display the depth 
+  /// </summary>
+  public static class depthHelper
+  {
+    // Globals used to determine the depths we are interested in
+    private const int MaxDepthDistance = 4000;
+    private const int MinDepth = 850;
+    private const int MaxDepthOffset = 3150;
+
+    /*
+     * "Slices" the depth image, returns a bitmap source with the pixels 
+     * colored so that no objects
+     * outside of a certain depth range are colored.
+     * Coloration taken predominantly from the microsoft depth tutorial, 
+     * slicing idea updated from Erik Climczak,
+     * at http://blogs.claritycon.com/blog/2012/11/blob-tracking-kinect-opencv-wpf/
+     **/
+    public static void depthSlice(this DepthImageFrame depthImage, WriteableBitmap bitmap, int min, int max)
+    {
+      int width = depthImage.Width;
+      int height = depthImage.Height;
+
+      //Raw depth data from kinect
+      short[] rawDepthData = new short[depthImage.PixelDataLength];
+      depthImage.CopyPixelDataTo(rawDepthData);
+
+      var pixels = new byte[height * width * 4];
+
+      //Constants for the positions of the relevant colors (Bgr32)
+      const int BlueIndex = 0;
+      const int GreenIndex = 1;
+      const int RedIndex = 2;
+
+      // Loop over two indicies, 
+      // one into the depth pixels, the other into the color
+      // pixel array that will be output 
+      for (int depthIndex = 0, colorIndex = 0;
+          depthIndex < rawDepthData.Length && colorIndex < pixels.Length;
+          depthIndex++, colorIndex += 4)
+      {
+
+        // Calculate the distance in mm 
+        // represented by the two depth bytes (bitshifting)
+        int depth = rawDepthData[depthIndex]
+            >> DepthImageFrame.PlayerIndexBitmaskWidth;
+        //Calculate the player value for the given index
+        int player = rawDepthData[depthIndex] &
+            DepthImageFrame.PlayerIndexBitmask;
+
+        // Map the distance to an intesity that can be represented in RGB
+        var intensity = calculateIntensity(depth);
+
+        if (depth > min && depth < max)
+        {
+          // Apply the intensity to the color channels
+          pixels[colorIndex + BlueIndex] = intensity; //blue
+          pixels[colorIndex + GreenIndex] = intensity; //green
+          pixels[colorIndex + RedIndex] = intensity; //red     
+
+          /* Colors a player (if it decides something is a player
+         * the relevant pixels will be gold)
+         * */
+          if (player > 0)
+          {
+            pixels[colorIndex + BlueIndex] = Colors.Gold.B;
+            pixels[colorIndex + GreenIndex] = Colors.Gold.G;
+            pixels[colorIndex + RedIndex] = Colors.Gold.R;
+          }
+        }
+      }
+
+      //Update the given writeableBitmap with the new pixel information
+      bitmap.WritePixels(
+        new Int32Rect(0, 0, bitmap.PixelWidth, bitmap.PixelHeight),
+        pixels, bitmap.PixelWidth * sizeof(int), 0);
+    }
+
+    /*
+     * Calculates the intentsity based on the given depth value of a pixel.
+     * This is a byte with a value between 0 and 255, 
+     * which will be mapped to RGB pixels and used to color the depthImage
+     * */
+    public static byte calculateIntensity(int depth)
+    {
+      int newMax = depth - MinDepth;
+      // Uses an offset to calculate values within the range
+      return (byte)(255 - (255 * Math.Max(depth - MinDepth, 0)
+          / (MaxDepthOffset)));
+    }
+  }
+}
